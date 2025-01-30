5c1f78e086c841324b38b2d0b479592c8782632e
badasukerubin <20546078+badasukerubin@users.noreply.github.com>
2025-01-30 13:55:14 +0000

    Fix styling

--- src/Items/Data/Contracts/NewItemDataContract.php
+++ src/Items/Data/Contracts/NewItemDataContract.php
@@ -6,6 +6,7 @@ namespace Returfs\Marketplace\External\Laravel\Items\Data\Contracts;
  * contract for a new item (request)
  *
  * todo: php 8.4 fixes this
+ *
  * @property-read ?string $uploadUuidKey
  */
 interface NewItemDataContract {}
--- src/Items/Events/Contracts/CreateNewItemEventContract.php
+++ src/Items/Events/Contracts/CreateNewItemEventContract.php
@@ -6,6 +6,7 @@ namespace Returfs\Marketplace\External\Laravel\Items\Events\Contracts;
  * @template T
  *
  * todo: php 8.4 fixes this
+ *
  * @property-read T $newItemData
  * @property-read ?string $socket
  */
